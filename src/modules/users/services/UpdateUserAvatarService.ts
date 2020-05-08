a16acb531261b42c7c6e720353b11716718b0a87
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2020-05-08 00:16:17 -0300

    Arquitetura e DDD

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,23 +1,27 @@
-import { getRepository } from 'typeorm';
 import path from 'path';
 import fs from 'fs';
+import uploadConfig from '@config/upload';
+import { injectable, inject } from 'tsyringe';
 
-import uploadConfig from '../config/upload';
+import AppError from '@shared/errors/AppError';
 
-import AppError from '../errors/AppError';
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
-import User from '../models/User';
-
-interface Request {
+interface IRequest {
   user_id: string;
   avatar_filename: string;
 }
 
+@injectable()
 class UpdateUserAvatarService {
-  public async execute({ user_id, avatar_filename }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
 
-    const user = await usersRepository.findOne(user_id);
+  public async execute({ user_id, avatar_filename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
       throw new AppError(
@@ -38,7 +42,7 @@ class UpdateUserAvatarService {
 
     user.avatar = avatar_filename;
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
