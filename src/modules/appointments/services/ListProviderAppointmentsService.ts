cbfbe298e4840d08d1ad8cc7fd89ea5984fb7fab
Jose Felipe Araujo <j.felipe.araujo@outlook.com>
2020-06-01 18:39:13 -0300

    Alterações

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -35,7 +35,7 @@ class ListProviderAppointmentsService {
       cacheKey,
     );
 
-    // let appointments;
+    // let appointments = null;
 
     if (!appointments) {
       appointments = await this.appointmentsRepository.findAllInDayFromProvider(
